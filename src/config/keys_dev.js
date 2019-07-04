4fe49cab87fa8133fc23f963926b7242e39bd24e
NhanLe1 <ntle1@pipeline.sbcc.edu>
2019-07-04 11:46:18 -0700

    fixing config file

--- src/config/keys_dev.js
+++ src/config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   client_id: "a3713a1e89214b1aa9c7a225de33f3b0",
   client_secret: "c7e8b8075cf646328c1a9c37edcea988", // Your secret
-  redirect_uri: "http://localhost:3000/callback/"
+  redirect_uri: "https://webify-react.herokuapp.com/callback"
 };
